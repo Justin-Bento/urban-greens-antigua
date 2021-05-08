837ab321ca2ab7a16fe0342aaff374a8fe9543ef
Justin-Bento <bentoj005@outlook.com>
2021-05-08 14:43:08 -0400

    end of testimonials

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -89,6 +89,34 @@ export default function Home() {
         </div>
       </section>
       {/* end of microgreen-benifits */}
+      <section className="testimonials">
+        <div className="section">
+          <div class="bg-gray-100 py-24">
+            <div class="w-full mx-auto md:w-5/6">
+              <div class="text-left md:text-center">
+                <p class="mb-8 text-xs font-bold tracking-widest text-green-600 uppercase">
+                  Why People Choose Urban Greens
+                </p>
+                <h1 class="mb-10 text-xl italic font-normal text-gray-800 md:leading-snug md:text-3xl">
+                  “We endeavour to build a truly human AND high-performing workplace, where everyone can do the best work of
+                  their lives. This product is a key part of facilitating this culture by being our one-stop-shop for all
+                  things performance development.”
+                </h1>
+              </div>
+              <div class="flex items-center justify-start md:justify-center">
+                <div class="avatar">
+                  <img src="/placeholder.jpg" alt="Photo of Praveen Juge" />
+                </div>
+                <div class="ml-4">
+                  <p class="mb-1 text-xs font-semibold tracking-widest text-gray-800 uppercase">Praveen Juge</p>
+                  <p class="text-xs font-semibold tracking-widest text-gray-500 uppercase">CEO, Birds</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* end of testimonials */}
       <Footer />
     </div>
   );
