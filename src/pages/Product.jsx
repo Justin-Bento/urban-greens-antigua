03247e011592d28feec503b91ad5313368ca9ec2
Justin-Bento <bentoj005@outlook.com>
2021-05-08 18:02:48 -0400

    created react products

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import products from '../lib/products.json';
+import Cards from '../components/Cards.jsx';
 
 export default function Product() {
   return (
-    <div>
-      <h1>Product</h1>
+    <div className="Products">
+      <div className="wrapper my-24">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-24">
+          {products.map((item, index) => {
+            return (
+              <div key={index}>
+                <Cards
+                  Media={item.Image}
+                  Price={item.Price}
+                  Headline={item.Microgreen}
+                  Body={item.Nutrients}
+                  Colour={item.Colour}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
