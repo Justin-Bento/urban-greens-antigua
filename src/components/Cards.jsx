752611d57c04e023d4c1fb4e12f48521ba7384be
Justin-Bento <bentoj005@outlook.com>
2021-05-09 09:33:37 -0400

    added tailwind linecamp

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -13,14 +13,20 @@ export default class Cards extends Component {
               loading="lazy"
             />
           </a>
-          <p className="mb-3 text-sm font-normal text-gray-500">{this.props.Price}</p>
+          <p className="mb-3 text-sm font-normal text-gray-500">
+            {this.props.Price}
+          </p>
           <h2 className="mb-2 text-lg font-semibold text-gray-900 capitalize">
             <a href="#" className="text-gray-900 hover:text-purple-700">
               {this.props.Headline}
             </a>
           </h2>
-          <p className="mb-3 text-sm font-normal text-gray-500">{this.props.Body}</p>
-          <p className="mb-3 text-sm font-normal text-gray-500">{this.props.Colour}</p>
+          <p className="line-clamp-3 mb-3 text-sm font-normal text-gray-500">
+            {this.props.Body}
+          </p>
+          <p className="mb-3 text-sm font-normal text-gray-500">
+            {this.props.Colour}
+          </p>
         </div>
       </div>
     );
