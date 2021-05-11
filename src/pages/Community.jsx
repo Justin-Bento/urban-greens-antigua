45bf4a77a13abd801d75aad7efc6605ee231c8b4
Justin-Bento <bentoj005@outlook.com>
2021-05-10 22:16:16 -0400

    created community page

---
+++ src/pages/Community.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Community() {
+  return (
+    <div className="community">
+      <div className="">
+        <h1>Community Page</h1>
+      </div>
+    </div>
+  );
+}
