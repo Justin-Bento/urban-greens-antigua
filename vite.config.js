c1f753e8fabce58ddef40f9795ac55ac19730408
Justin-Bento <bentoj005@outlook.com>
2021-05-11 14:53:38 -0400

    allow access to netlify

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import WindiCSS from 'vite-plugin-windicss';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +10,19 @@ export default defineConfig({
     WindiCSS(),
     // <--
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src'),
+    },
+  },
+  server: {
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8888/.netlify/functions',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
