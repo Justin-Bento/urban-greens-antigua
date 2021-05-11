df858c3f7286f61b6888b49f5823b91ae131bcde
Justin-Bento <bentoj005@outlook.com>
2021-05-10 22:15:26 -0400

    changed type scale and spacing

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -8,25 +8,19 @@ export default class Cards extends Component {
           <a href="#">
             <img
               src={`../src/assets/products/${this.props.Media}`}
-              className="object-contain w-full h-56 mb-5 bg-center rounded"
+              className="object-contain w-full h-56 bg-center rounded mb-1"
               alt="Kutty"
               loading="lazy"
             />
           </a>
-          <p className="mb-3 text-sm font-normal text-gray-500">
-            {this.props.Price}
-          </p>
-          <h2 className="mb-2 text-lg font-semibold text-gray-900 capitalize">
-            <a href="#" className="text-gray-900 hover:text-purple-700">
+          <p className="overline mb-1">{this.props.Price}</p>
+          <h2 className="subtitle-1">
+            <a href="#" className="mb-1 hover:text-green-700">
               {this.props.Headline}
             </a>
           </h2>
-          <p className="line-clamp-3 mb-3 text-sm font-normal text-gray-500">
-            {this.props.Body}
-          </p>
-          <p className="mb-3 text-sm font-normal text-gray-500">
-            {this.props.Colour}
-          </p>
+          <p className="line-clamp-3 body-2 mb-2">{this.props.Body}</p>
+          <p className="caption">{this.props.Colour}</p>
         </div>
       </div>
     );
