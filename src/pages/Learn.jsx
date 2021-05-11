2ab0fc490a5680844704f78136a6194f87828456
Justin-Bento <bentoj005@outlook.com>
2021-05-10 22:17:00 -0400

    added frequently asked questions

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import Footer from '../components/Footer.jsx';
+import data from '../lib/data.json';
 
 export default function Learn() {
   return (
-    <div className="Learn">
-      <div className="wrapper my-24">
-        <h1>Learn</h1>
+    <section className="learn">
+      <div className="wrapper">
+        <div className="my-24">
+          <h1 className="headline-2">Frequently Asked Questions</h1>
+          {data.faqs.map((item, index) => {
+            return (
+              <div
+                className="grid grid-cols-1 md:grid-cols-2 items-center my-12"
+                key={index}
+              >
+                <div className="divide-y divide-light-blue-400">
+                  <h3 className="subtitle-1">{item.headline}</h3>
+                </div>
+                <div className="">
+                  <p>{item.body}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+      <Footer />
+    </section>
   );
 }
