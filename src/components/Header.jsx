8d93e2602722ccef479acab19d18f492233566af
Justin-Bento <bentoj005@outlook.com>
2021-05-13 10:47:35 -0400

    updated links to button  links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,16 +23,16 @@ export default class Header extends Component {
                 <div className="menu"></div>
               </div>
               <div className="col-2">
-                <Link to="/product" className="md:p-2 hover:text-green-600">
+                <Link to="/product" className="btn-link">
                   Products
                 </Link>
-                <Link to="/learn" className="md:p-2 hover:text-green-600">
+                <Link to="/learn" className="btn-link">
                   Learn
                 </Link>
-                <Link to="/about" className="md:p-2 hover:text-green-600">
+                <Link to="/about" className="btn-link">
                   About
                 </Link>
-                <Link to="/contact" className="md:p-2 hover:text-green-600">
+                <Link to="/contact" className="btn-link">
                   Contact
                 </Link>
               </div>
