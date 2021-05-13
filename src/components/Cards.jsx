e3bf0770e5baed887b3711edf86c5285e3ae5efe
Justin-Bento <bentoj005@outlook.com>
2021-05-13 18:00:46 -0400

    removed validatedomnesting

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Cards extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <a href="#">
-            <img
-              src={`${this.props.Media}`}
-              className="object-contain w-full h-56 bg-center rounded mb-1"
-              alt="Kutty"
-              loading="lazy"
-            />
-          </a>
-          <p className="overline text-green-600 mb-1">{this.props.Price}</p>
-          <h2 className="subtitle-1">
-            <a href="#" className="mb-1 hover:text-green-700">
-              {this.props.Headline}
-            </a>
-          </h2>
-          <p className="line-clamp-3 body-2 mb-2">{this.props.Body}</p>
-          <p className="caption">{this.props.Colour}</p>
-        </div>
+      <div id="card">
+        <img
+          src={`${this.props.Media}`}
+          className="object-contain w-full h-56 bg-center rounded mb-1"
+          alt="Kutty"
+          loading="lazy"
+        />
+        <p className="overline text-green-600 mb-1">{this.props.Price}</p>
+        <h2 className="subtitle-1">{this.props.Headline}</h2>
+        <p className="line-clamp-3 body-2 mb-2">{this.props.Body}</p>
+        <p className="caption">{this.props.Colour}</p>
       </div>
     );
   }
