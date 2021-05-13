05400363f009f2a07b4d1dce80f042e8ad39a3b2
Justin-Bento <bentoj005@outlook.com>
2021-05-13 14:02:08 -0400

    updated spacing and font-weight

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,16 +23,16 @@ export default class Header extends Component {
                 <div className="menu"></div>
               </div>
               <div className="col-2">
-                <Link to="/product" className="btn-link">
+                <Link to="/product" className="btn-link font-normal">
                   Products
                 </Link>
-                <Link to="/learn" className="btn-link">
+                <Link to="/learn" className="btn-link font-normal m-2">
                   Learn
                 </Link>
-                <Link to="/about" className="btn-link">
+                <Link to="/about" className="btn-link font-normal m-2">
                   About
                 </Link>
-                <Link to="/contact" className="btn-link">
+                <Link to="/contact" className="btn-link font-normal m-2">
                   Contact
                 </Link>
               </div>
