e2647557283721f014c3e42bba02634bddfe1bb3
Justin-Bento <bentoj005@outlook.com>
2021-05-13 17:36:27 -0400

    created mobile nav that  shows hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,41 +4,56 @@ import { Link } from 'react-router-dom';
 export default class Header extends Component {
   render() {
     return (
-      <header className="sticky top-0 z-90">
-        <div className="bg-white shadow-sm py-2">
-          <div className="wrapper">
-            <div className="md:flex md:justify-between  md:items-center">
-              <div className="col-1">
-                <div className="logo">
-                  <Link to="/">
-                    <img
-                      src="../../assets/logo/urban-greens-logo.png"
-                      alt=""
-                      width="96px"
-                      height="96px"
-                      loading="lazy"
-                    />
-                  </Link>
-                </div>
-                <div className="menu"></div>
+      <header className="sticky top-0 z-90 bg-white shadow-sm">
+        <nav className="wrapper mx-auto md:justify-between">
+          <div className="md:container md:flex md:justify-between md:items-center py-6">
+            <div className="flex justify-between items-center">
+              <div className="1">
+                <Link to="/">
+                  <img
+                    src="/assets/logo/urban-greens-logo.png"
+                    alt=""
+                    width="86px"
+                    height="86px"
+                  />
+                </Link>
               </div>
-              <div className="col-2">
-                <Link to="/product" className="btn-link font-normal">
+              <div className="text-2xl md:hidden">
+                <ion-icon
+                  name="menu"
+                  id="hamburgerbtn"
+                  className=" bg-purple-800"
+                ></ion-icon>
+              </div>
+            </div>
+
+            <div
+              className="hidden md:flex flex-row md:items-center md:inline-block"
+              id="mobileMenu"
+            >
+              <div className="">
+                <Link to="/product" className="p-2 m-2">
                   Products
                 </Link>
-                <Link to="/learn" className="btn-link font-normal m-2">
+              </div>
+              <div className="">
+                <Link to="/learn" className="p-2 m-2">
                   Learn
                 </Link>
-                <Link to="/about" className="btn-link font-normal m-2">
+              </div>
+              <div className="">
+                <Link to="/about" className="p-2 m-2">
                   About
                 </Link>
-                <Link to="/contact" className="btn-link font-normal m-2">
+              </div>
+              <div className="/contact">
+                <Link to="" className="p-2 m-2">
                   Contact
                 </Link>
               </div>
             </div>
           </div>
-        </div>
+        </nav>
       </header>
     );
   }
