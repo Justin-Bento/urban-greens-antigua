0053499c1531db481b0444e6e9d2dd3d150be7fe
Justin-Bento <bentoj005@outlook.com>
2021-05-14 09:25:55 -0400

    changed case

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 
 export default function Product() {
   return (
-    <div className="Products">
+    <div className="products">
       <div className="wrapper my-24">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-24">
           {products.map((item, index) => {
             return (
               <div key={index}>
-                <Link to={`/product/${item.id}`}>
+                <Link to={`/products/${item.id}`}>
                   <Cards
                     Media={`../../assets/products/${item.Image}`}
                     Price={item.Price}
