43bb81e952d1b0c53ebc2cfacc705e15274e72db
Justin-Bento <bentoj005@outlook.com>
2021-05-18 16:03:51 -0400

    created a card component

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 export default class Cards extends Component {
   render() {
     return (
-      <div id="card">
+      <div
+        id="card"
+        className="transition duration-75 hover:shadow-sm hover:border p-4 rounded"
+      >
         <img
           src={`${this.props.Media}`}
           className="object-contain w-full h-56 bg-center rounded mb-1"
@@ -14,7 +17,7 @@ export default class Cards extends Component {
         <p className="overline text-green-600 mb-1">{this.props.Price}</p>
         <h2 className="subtitle-1">{this.props.Headline}</h2>
         <p className="line-clamp-3 body-2 mb-2">{this.props.Body}</p>
-        <p className="caption">{this.props.Colour}</p>
+        <p className="caption capitalize">{this.props.Colour}</p>
       </div>
     );
   }
