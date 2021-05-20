b2945db9cfb4e72309b0fb69feebec8c6e87ecdc
Justin-Bento <bentoj005@outlook.com>
2021-05-20 16:45:39 -0400

    added custom style for grid-3

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,7 +7,7 @@ export default function Product() {
   return (
     <div className="products">
       <div className="wrapper my-24">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-24">
+        <div className="grid-3">
           {products.map((product) => {
             return (
               <div key={product.id}>
