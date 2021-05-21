0e53ebcb73275a7c79f000a260f06e6dd5ab2889
Justin-Bento <bentoj005@outlook.com>
2021-05-21 10:57:37 -0400

    fetch, maped and hooked product data

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,23 +1,48 @@
-import React from 'react';
-import products from '../../public/lib/products.json';
+import React, { useState, useEffect } from 'react';
 import Cards from '../components/Cards.jsx';
 import { Link } from 'react-router-dom';
 
 export default function Product() {
+  // Empty array in useState!
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    fetch('/public/lib/products.json')
+      .then((res) => res.json())
+      .then((datas) => {
+        setData(datas);
+        console.log(datas);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  if (loading) {
+    return <p>Data is loading...</p>;
+  }
+  if (error || !Array.isArray(data)) {
+    return <p>There was an error loading your data!</p>;
+  }
   return (
     <div className="products">
       <div className="wrapper my-24">
         <div className="grid-3">
-          {products.map((product) => {
+          {data.map((data) => {
             return (
-              <div key={product.id}>
-                <Link to={`/products/${product.id}`}>
+              <div key={data.id}>
+                <Link to={`/products/${data.id}`}>
                   <Cards
-                    Media={`../../assets/products/${product.Image}`}
-                    Price={product.Price}
-                    Headline={product.Microgreen}
-                    Body={product.Nutrients}
-                    Colour={product.Colour}
+                    Media={`../../assets/products/${data.Image}`}
+                    Price={data.Price}
+                    Headline={data.Microgreen}
+                    Body={data.Nutrients}
+                    Colour={data.Colour}
                   />
                 </Link>
               </div>
