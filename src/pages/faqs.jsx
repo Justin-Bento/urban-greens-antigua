0a4f20ef2a28c35ff113e85a76c1af989d05191d
Justin <bentoj005@outlook.com>
2021-06-14 23:48:32 -0400

    removed map function and added local data

--- src/pages/faqs.jsx
+++ src/pages/faqs.jsx
@@ -7,18 +7,30 @@ export default function faqs() {
       <div className="wrapper">
         <div className="spacing">
           <h1 className="headline-2">Frequently Asked Questions</h1>
-          {data.faqs.map((item, index) => {
-            return (
-              <div className="spacing-md" key={item.id}>
-                <div className="">
-                  <h3 className="subtitle-1">{item.headline}</h3>
-                </div>
-                <div className="">
-                  <p>{item.body}</p>
-                </div>
-              </div>
-            );
-          })}
+          <div className="spacing-sm">
+            <h3 className="subtitle-1">What Are Microgreens?</h3>
+            <p className="body-1">Microgreens grow from sprouts and are young vegetable greens harvested just after the cotyledon or first true leaves from the seedling have developed.   Microgreens grow from sprouts and are the first true leaves to emerge.</p>
+          </div>
+          <div className="spacing-sm">
+            <h3 className="subtitle-1">How are Microgreens used?</h3>
+            <p className="body-1">Microgreens are aromatic and highly nutritious and come in a wide variety of flavors, textures and colors.  They are eaten on their own, used to add flavor and/or texture to salads or dishes, used in sandwiches and wraps, used in juices or smoothies or as a  garnish or decoration for plates.</p>
+          </div>
+          <div className="spacing-sm">
+            <h3 className="subtitle-1">What are the health benefits?</h3>
+            <p className="body-1">Microgreens provide minerals, nutrients and fiber which help to prevent a range of diseases, manage weight and boost both mental and physical health and well-being. Studies have shown that microgreens are loaded with nutrients and vitamins such as vitamin C, E, and K, lutein and beta-carotene, 40 fold that of the mature leaves of the same plants</p>
+          </div>
+          <div className="spacing-sm">
+            <h3 className="subtitle-1">Where Can you purchase microgreens?</h3>
+            <p className="body-1">Epicurean on Friars Hill and in Jolly Harbour, as well as Covent Gardens in English Harbour carry some of our products.   For special orders or a wider variety, including  more exotic microgreens, you can order directly from us.</p>
+          </div>
+          <div className="spacing-sm">
+            <h3 className="subtitle-1">Is Your Packaging recyclable?</h3>
+            <p className="body-1">Our packaging is recyclable.</p>
+          </div>
+          <div className="spacing-sm">
+            <h3 className="subtitle-1">What Is Your Contact Infromation?</h3>
+            <p className="body-1">Email address:  urbangreensanu@gmail.com.  Telephone/WhatsApp:  (268)720-8474.</p>
+          </div>
         </div>
       </div>
     </section>
