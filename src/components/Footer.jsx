3d9fc536cfe08e7efdc8baba4f627585733e1abf
Justin Bento <bento.a.justin@gmail.com>
2021-11-30 16:16:10 -0500

    removed tailwind syntaxt with custom components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,75 +1,74 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Footer extends Component {
   render() {
     return (
       <div>
-        <footer className="px-4 py-20 text-gray-100 bg-green-800">
+        <footer className="footerBackground">
           <div className="wrapper">
-            <div className="grid grid-cols-2 gap-4 mb-4 md:grid-cols-4">
-              <Link to="/" className="col-span-2 md:col-span-1">
-                <img
-                  src="../../assets/logo/urban-greens-logo-white.png"
-                  alt=""
-                  className="object-cover bg-contain w-20 h-20 rounded"
-                  loading="lazy"
-                  width="96px"
-                  height="96px"
-                />
+            <div className="footerGrid">
+              <Link to="/" className="footerGridOrder">
+                <img src={footer.logoWhite} alt="" className="footerLogo" loading="lazy" width="96px" height="96px" />
               </Link>
               <nav className="mb-8">
                 <p className="mb-4 subtitle-1 text-gray-50">Pages</p>
-                <Link to="/products" className="flex mb-4 body-2 text-gray-100">
-                  Products
-                </Link>
-                <Link to="/faqs" className="flex mb-4 body-2 text-gray-100">
-                  FAQ's
-                </Link>
-                <Link to="/about" className="flex mb-4 body-2 text-gray-100">
-                  About
-                </Link>
-                <Link to="/contact" className="flex body-2 text-gray-100">
-                  Contact
-                </Link>
+                <div className="flex flex-col">
+                  <Link to="/products" className="footerText">
+                    Products
+                  </Link>
+                  <Link to="/faqs" className="footerText">
+                    FAQ's
+                  </Link>
+                  <Link to="/about" className="footerText">
+                    About
+                  </Link>
+                  <Link to="/contact" className="footerText">
+                    Contact
+                  </Link>
+                </div>
               </nav>
-              <nav className="mb-8">
+              <div className="mb-8">
                 <p className="mb-4 subtitle-1 text-gray-50">About</p>
-                <Link to="/about" className="flex mb-4 body-2 text-gray-100">
-                  Company
-                </Link>
-                <Link to="/about" className="flex mb-4 body-2 text-gray-100">
-                  Story
-                </Link>
-                <Link to="/about" className="flex mb-4 body-2 text-gray-100">
-                  Values
-                </Link>
-              </nav>
-              <nav className="mb-8">
+                <div className="flex flex-col">
+                  <Link to="/about" className="footerText">
+                    Company
+                  </Link>
+                  <Link to="/about" className="footerText">
+                    Story
+                  </Link>
+                  <Link to="/about" className="footerText">
+                    Values
+                  </Link>
+                </div>
+              </div>
+              <div className="mb-8">
                 <p className="mb-4 subtitle-1 text-gray-50">Contact</p>
-                <a
-                  href="https://www.facebook.com/UrbanGreensANU/"
-                  className="flex mb-4 body-2 text-gray-100"
-                >
-                  Facebook
-                </a>
-                <a
-                  href="https://instagram.com/urbangreensanu?utm_medium=copy_link"
-                  className="flex mb-4 body-2 text-gray-100"
-                >
-                  Instagram
-                </a>
-                <a href="#" className="flex mb-4 body-2 text-gray-100">
-                  Email
-                </a>
-              </nav>
+                <div className="flex flex-col">
+                  <a href={footer.socialMedia.facebook} className="footerText">
+                    Facebook
+                  </a>
+                  <a href={footer.socialMedia.instgram} className="footerText">
+                    Instagram
+                  </a>
+                  <a href="#" className="footerText">
+                    Email
+                  </a>
+                </div>
+              </div>
             </div>
-            <p className="subtitle-2 text-green-100 md:text-center">
-              © Copyright 2021 Urban Greens Inc. All Rights Reserved.
-            </p>
+            <p className="footerText text-center">© Copyright 2021 Urban Greens Inc. All Rights Reserved.</p>
           </div>
         </footer>
       </div>
     );
   }
 }
+
+let footer = {
+  logoWhite: "../../assets/logo/urban-greens-logo-white.png",
+  socialMedia: {
+    facebook: "https://www.facebook.com/UrbanGreensANU/",
+    instgram: "https://instagram.com/urbangreensanu?utm_medium=copy_link",
+  },
+};
