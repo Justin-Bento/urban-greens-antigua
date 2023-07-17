ef7e1dc974dba7a380486bdd1c2a0d3329f9918d
Justin Bento <bento.a.justin@gmail.com>
2023-07-17 13:01:54 -0400

    added Link and buttons to emphases its importance

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { Button } from '../components/Button';
+import { Button } from '../Button';
 
 export default function Hero() {
   return (
     <section className="text-start sm:text-center">
       <h1 className="text-5xl font-extrabold tracking-tight capitalize scroll-m-20">Sustainable Microgreens, Unbeatable Flavor!</h1>
       <p className="text-lg leading-7 max-w-[80ch] mx-auto mt-4 lg:text-xl text-gray-950/80 tracking-wide">Our microgreens are packed with nutrients and flavor, making them the perfect addition to your meals. From salads to smoothies, sandwiches to sushi, the possibilities are endless. Easily incorporate microgreens into your diet without leaving your home.</p>
+      <div className="flex flex-col justify-start gap-8 mt-6 sm:justify-center sm:flex-row">
+        <Link to="/products">
+          <Button size="lg">Check Out Greens</Button>
+        </Link>
+        <Link to="/about">
+          <Button variant="outline" size="lg">About Urban Greens</Button>
+        </Link>
+      </div>
       <img src="/public/assets/home/hero.png" alt="" className="object-fill object-center w-full mt-12 rounded sm:rounded-lg aspect-square md:aspect-video" />
       <caption className="w-full mt-4 text-sm italic tracking-wider text-gray-900/70">Appetizers at Hodges Bay Club garnished with our Island Microgreens. Bravo Chef Leon for this beautiful presentation!</caption>
     </section>
