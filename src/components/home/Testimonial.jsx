bace5c8665908f964c19474cb612402eb35a23fb
Justin Bento <bento.a.justin@gmail.com>
2023-07-17 12:20:28 -0400

    moved Testimonial section into component

---
+++ src/components/home/Testimonial.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Testimonial() {
+  return (
+    <>
+      <section>
+        <h5>Hello Urban Greens Section 5</h5>
+      </section>
+    </>
+  );
+}
