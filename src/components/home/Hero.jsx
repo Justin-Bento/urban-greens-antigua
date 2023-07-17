2a0d1cf2b84cb15d0229035c4afd431ebed9673d
Justin Bento <bento.a.justin@gmail.com>
2023-07-17 12:57:45 -0400

    alinged text left on mobile and centered text on tablet

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <section>
+    <section className="text-start sm:text-center">
       <h1>Sustainable Microgreens, Unbeatable Flavor!</h1>
       <p>Our microgreens are packed with nutrients and flavor, making them the perfect addition to your meals. From salads to smoothies, sandwiches to sushi, the possibilities are endless. Easily incorporate microgreens into your diet without leaving your home.</p>
       <img src="/public/assets/home/hero.png" alt="" />
