5b80ab119d19f42d689b5e5bb05597dff544818a
Justin Bento <bento.a.justin@gmail.com>
2023-07-17 12:37:15 -0400

    updated footer with consistent typography and rhythm=

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,74 +1,99 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom';
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <div>
-        <footer className="footerBackground">
-          <div className="wrapper">
-            <div className="footerGrid">
-              <Link to="/" className="footerGridOrder">
-                <img src={footer.logoWhite} alt="" className="footerLogo" loading="lazy" width="96px" height="96px" />
-              </Link>
-              <nav className="mb-8">
-                <p className="mb-4 subtitle-1 text-gray-50">Pages</p>
-                <div className="flex flex-col">
-                  <Link to="/products" className="footerText">
-                    Products
-                  </Link>
-                  <Link to="/faqs" className="footerText">
-                    FAQ's
-                  </Link>
-                  <Link to="/about" className="footerText">
-                    About
-                  </Link>
-                  <Link to="/contact" className="footerText">
-                    Contact
-                  </Link>
-                </div>
-              </nav>
-              <div className="mb-8">
-                <p className="mb-4 subtitle-1 text-gray-50">About</p>
-                <div className="flex flex-col">
-                  <Link to="/about" className="footerText">
-                    Company
-                  </Link>
-                  <Link to="/about" className="footerText">
-                    Story
-                  </Link>
-                  <Link to="/about" className="footerText">
-                    Values
-                  </Link>
-                </div>
-              </div>
-              <div className="mb-8">
-                <p className="mb-4 subtitle-1 text-gray-50">Contact</p>
-                <div className="flex flex-col">
-                  <a href={footer.socialMedia.facebook} className="footerText">
-                    Facebook
-                  </a>
-                  <a href={footer.socialMedia.instgram} className="footerText">
-                    Instagram
-                  </a>
-                  <a href="#" className="footerText">
-                    Email
-                  </a>
-                </div>
-              </div>
-            </div>
-            <p className="footerText text-center">© Copyright 2021 Urban Greens Inc. All Rights Reserved.</p>
-          </div>
-        </footer>
-      </div>
-    );
-  }
+
+const navigation = {
+  solutions: [
+    { name: 'Facebook', href: '#' },
+    { name: 'Instagram', href: '#' },
+    { name: 'Twitter', href: '#' },
+    { name: "WhatsApp", href: '#' },
+  ],
+  support: [
+    { name: 'Pricing', href: '#' },
+    { name: 'Delivery', href: '#' },
+    { name: 'Testimonials', href: '#' },
+  ],
+  company: [
+    { name: 'About', href: '#' },
+    { name: 'Products', href: '#' },
+    { name: 'Contact', href: '#' },
+    { name: 'Press', href: '#' },
+  ],
+  legal: [
+    { name: 'Claim', href: '#' },
+    { name: 'Privacy', href: '#' },
+    { name: 'Terms', href: '#' },
+  ],
 }
 
-let footer = {
-  logoWhite: "../../assets/logo/urban-greens-logo-white.png",
-  socialMedia: {
-    facebook: "https://www.facebook.com/UrbanGreensANU/",
-    instgram: "https://instagram.com/urbangreensanu?utm_medium=copy_link",
-  },
-};
+export default function Footer() {
+  return (
+    <footer className="bg-primary-700" aria-labelledby="footer-heading">
+      <h6 id="footer-heading" className="sr-only">
+        Footer
+      </h6>
+      <div className="container p-4 py-16 mx-auto">
+        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+        <div className="relative w-auto h-32 aspect-square">
+        <img src="../../assets/logo/urban-greens-logo-white.png" alt="" className="object-fit"/>
+        </div>
+          <div className="grid grid-cols-2 gap-8 mt-16 xl:col-span-2 xl:mt-0">
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-semibold leading-6 text-primary-50">Social Media</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {navigation.solutions.map((item) => (
+                    <li key={item.name}>
+                      <Link to={item.href} className="text-sm leading-7 text-primary-50/80">
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold leading-6 text-primary-50">Support</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {navigation.support.map((item) => (
+                    <li key={item.name}>
+                      <Link to={item.href} className="text-sm leading-7 text-primary-50/80">
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-semibold leading-6 text-primary-50">Company</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {navigation.company.map((item) => (
+                    <li key={item.name}>
+                      <Link to={item.href} className="text-sm leading-7 text-primary-50/80">
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold leading-6 text-primary-50">Legal</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {navigation.legal.map((item) => (
+                    <li key={item.name}>
+                      <Link to={item.href} className="text-sm leading-7 text-primary-50/80">
+                        {item.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
