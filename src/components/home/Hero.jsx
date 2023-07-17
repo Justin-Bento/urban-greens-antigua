61cff6ad4378305340699af1d830d6fe7de02486
Justin Bento <bento.a.justin@gmail.com>
2023-07-17 12:57:07 -0400

    added new content to the homepage

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Hero() {
   return (
     <section>
-      <h1>Hello Urban Greens Section 1</h1>
+      <h1>Sustainable Microgreens, Unbeatable Flavor!</h1>
+      <p>Our microgreens are packed with nutrients and flavor, making them the perfect addition to your meals. From salads to smoothies, sandwiches to sushi, the possibilities are endless. Easily incorporate microgreens into your diet without leaving your home.</p>
+      <img src="/public/assets/home/hero.png" alt="" />
+      <caption>Appetizers at Hodges Bay Club garnished with our Island Microgreens. Bravo Chef Leon for this beautiful presentation!</caption>
     </section>
   )
 }
