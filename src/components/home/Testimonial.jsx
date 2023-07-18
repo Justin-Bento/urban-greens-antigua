6dcae81df9936433316cf1c1e8860e052b08adba
Justin Bento <bento.a.justin@gmail.com>
2023-07-18 12:53:37 -0400

    styled testimoinal container

--- src/components/home/Testimonial.jsx
+++ src/components/home/Testimonial.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Testimonial() {
   return (
     <>
-      <section>
-        <h5>Hello Urban Greens Section 5</h5>
+      <section className="relative px-6 py-24 overflow-hidden text-left isolate sm:rounded-3xl sm:px-16 rounded-xl bg-primary-200 dark:bg-primary-900">
+        <h5 className="pb-2 mt-10 text-3xl font-extrabold tracking-tight md:text-center text-start transtion-colors md:tex scroll-m-20 first:mt-0">Hello Urban Greens Section 5</h5>
       </section>
     </>
   );
