428926cebe79a1076e2f47e691b4bd46946eb879
Justin Bento <bento.a.justin@gmail.com>
2023-07-18 12:53:07 -0400

    created a usage section

--- src/components/home/Usage.jsx
+++ src/components/home/Usage.jsx
@@ -2,8 +2,19 @@ import React from "react";
 
 export default function Usage() {
   return (
-    <section>
-      <h6>Hello Urban Greens Section 6</h6>
+    <section className="flex gap-8 justify-evenly">
+      <div className="max-w-prose">
+      <h6 className="mb-2 text-2xl font-semibold tracking-tight scroll-m-20">Understanding Our Package?</h6>
+        <p className="leading-7 tracking-wide">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, soluta nam eius iure enim praesentium voluptatem, sint delectus non optio, maiores quae voluptates voluptate dignissimos omnis earum cupiditate dolore.</p>
+      </div>
+      <div className="max-w-prose">
+      <h6 className="mb-2 text-2xl font-semibold tracking-tight scroll-m-20">Understanding Our Package?</h6>
+        <p className="leading-7 tracking-wide">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, soluta nam eius iure enim praesentium voluptatem, sint delectus non optio, maiores quae voluptates voluptate dignissimos omnis earum cupiditate dolore.</p>
+      </div>
+      <div className="max-w-prose">
+      <h6 className="mb-2 text-2xl font-semibold tracking-tight scroll-m-20">Understanding Our Package?</h6>
+        <p className="leading-7 tracking-wide">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, soluta nam eius iure enim praesentium voluptatem, sint delectus non optio, maiores quae voluptates voluptate dignissimos omnis earum cupiditate dolore.</p>
+      </div>
     </section>
   );
 }
