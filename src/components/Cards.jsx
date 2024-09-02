796d257159270cdb33bc2c6cb727952ca1b05c21
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-09-02 10:21:32 -0500

    Fixed image resolution by giving it a clear aspect ratio.

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,7 +4,7 @@ export default class Cards extends Component {
   render() {
     return (
       <div id="card" className="transition duration-150 border border-transparent hover:shadow-sm hover:border-gray-100 p-4 rounded">
-        <img src={`${this.props.Media}`} className="object-contain w-full h-56 bg-center rounded mb-1" alt="Kutty" loading="lazy" />
+        <img src={`${this.props.Media}`} className="w-full object-contain aspect-video bg-center rounded mb-1" alt="Kutty" loading="lazy" />
         <p className="text-xs uppercase text-green-600 mb-1">{this.props.Price}</p>
         <h2 className="align-middle antialiased text-base text-gray-900 font-bold capitalize leading-normal">{this.props.Headline}</h2>
         <p className="line-clamp-3 align-middle antialiased text-sm max-w-full text-gray-800 leading-5 tracking-wide mb-2">{this.props.Body}</p>
