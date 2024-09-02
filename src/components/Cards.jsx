b3e284b286ed79edb9e31cb1e9fd5b57e14a6949
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-09-02 10:27:55 -0500

    Added border to card and copied card to follow shadcn library.

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,11 +3,12 @@ import React, { Component } from "react";
 export default class Cards extends Component {
   render() {
     return (
-      <div id="card" className="transition duration-150 border border-transparent hover:shadow-sm hover:border-gray-100 p-4 rounded">
+      <div id="card" className="rounded-lg border shadow-xs space-y-2 p-6 hover:border-green-600">
         <img src={`${this.props.Media}`} className="w-full object-contain aspect-video bg-center rounded mb-1" alt="Kutty" loading="lazy" />
         <p className="text-xs uppercase text-green-600 mb-1">{this.props.Price}</p>
-        <h2 className="align-middle antialiased text-base text-gray-900 font-bold capitalize leading-normal">{this.props.Headline}</h2>
-        <p className="line-clamp-3 align-middle antialiased text-sm max-w-full text-gray-800 leading-5 tracking-wide mb-2">{this.props.Body}</p>
+        <h2 className="text-xl font-semibold leading-none tracking-tight align-middle subpixel-antialiased">{this.props.Headline}</h2>
+        <p className="text-sm tracking-wide leading-5 line-clamp-3 text-gray-600">{this.props.Body}</p>
+        <span className="block border border-gray-200 w-20"></span>
         <p className="align-middle subpixel-antialiased text-sm text-gray-500 leading-normal capitalize">{this.props.Colour}</p>
       </div>
     );
