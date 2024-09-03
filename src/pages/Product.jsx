eeb233d087d33f9ed892d7f89b3214128b87ea11
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-09-03 18:20:57 -0400

    Removed Card import as the file no longer exists.

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Cards from "../components/Cards.jsx";
 import { Link } from "react-router-dom";
 import NotFound from "../components/NotFound.jsx";
 import Loading from "../components/Loading.jsx";
@@ -46,16 +45,26 @@ export default function Product() {
           <span className="block border-2 w-24 border-green-700 rounded-full mt-6"></span>
         </section>
         <section className="grid grid-cols-1 gap-x-6 gap-y-8 md:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-          {data.map((data) => {
+          {data.map((data, index) => {
             return (
-              <Link key={data.id} to={`/products/${data.id}`}>
-                <Cards
-                  Media={`../../assets/products/${data.Image}`}
-                  Price={data.Price}
-                  Headline={data.Microgreen}
-                  Body={data.Nutrients}
-                  Colour={data.Colour}
-                />
+              <Link key={index} to={`/products/${data.id}`}>
+                <div id="card" className="rounded-lg border shadow-xs space-y-2 p-6 hover:border-green-600">
+                  <img
+                    src={`../../assets/products/${data.Image}`}
+                    className="w-full object-contain aspect-video bg-center rounded mb-1"
+                    alt={`Microgreens Name: ${data.Microgreen}`}
+                    loading="lazy"
+                  />
+                  <p className="text-xs uppercase text-green-600 mb-1">{data.Price}</p>
+                  <h2 className="text-xl font-semibold leading-none tracking-tight align-middle subpixel-antialiased">
+                    {data.Microgreen}
+                  </h2>
+                  <p className="text-sm tracking-wide leading-5 line-clamp-3 text-gray-600">{data.Nutrients}</p>
+                  <span className="block border border-gray-200 w-20"></span>
+                  <p className="align-middle subpixel-antialiased text-sm text-gray-500 leading-normal capitalize">
+                    {data.Colour}
+                  </p>
+                </div>
               </Link>
             );
           })}
