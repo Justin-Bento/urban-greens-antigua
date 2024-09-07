fdb231e02d61c4250d6e1af2757bb02d9bc7da03
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-09-07 13:36:37 -0400

    Added TItle and Description prop type to host static headlines.

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default function Title() {
+export default function Title(props) {
   return (
     <section className="my-12">
       <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight">
-        {/*  */}
+        {props.Headline}
       </h1>
       <p className="leading-5 tracking-wide text-balance mt-2">
-        {/*  */}
+        {props.Description}
       </p>
       <span className="block border-2 w-24 border-green-700 rounded-full mt-6"></span>
     </section>
