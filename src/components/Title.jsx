0b929b05f478c8a993095b1e71ec35da5b49b11f
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2024-09-07 13:32:00 -0400

    Transferred heading layout from products page to the Title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Title() {
   return (
-    <div>Title</div>
-  )
+    <section className="my-12">
+      <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight">
+        {/*  */}
+      </h1>
+      <p className="leading-5 tracking-wide text-balance mt-2">
+        {/*  */}
+      </p>
+      <span className="block border-2 w-24 border-green-700 rounded-full mt-6"></span>
+    </section>
+  );
 }
