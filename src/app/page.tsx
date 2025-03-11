42dcea0499bbc9d7c534697a3dc6bda820c2c4b6
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-11 13:58:59 -0400

    Installed shadcn/ui for Next.js.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
 export default function Page() {
-  return <h1>Hello Next.js!</h1>;
+  return (
+    <>
+      <main className="">
+        <section className="">
+          <h1 className="">Hello Section #1</h1>
+          <p className=""></p>
+        </section>
+        <section className="">
+          <h2 className="">Hello Section #2</h2>
+          <p className=""></p>
+        </section>
+        <section className="">
+          <h3 className="">Hello Section #3</h3>
+          <p className=""></p>
+        </section>
+        <section className="">
+          <h4 className="">Hello Section #4</h4>
+          <p className=""></p>
+        </section>
+        <section className="">
+          <h5 className="">Hello Section #5</h5>
+          <p className=""></p>
+        </section>
+        <section className="">
+          <h6 className="">Hello Section #6</h6>
+          <p className=""></p>
+        </section>
+      </main>
+    </>
+  );
 }
