e8ebafb2184936e1e7a5daba0a4a01394ed739c4
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-11 18:01:53 -0400

    Created a header component to see the start of the website

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card, CardContent } from "./ui/card";
+
+export default function Navigation() {
+  return (
+    <header>
+      <Card className="rounded-none">
+        <CardContent className="container mx-auto">
+          <p className="">Start of the website.</p>
+        </CardContent>
+      </Card>
+    </header>
+  );
+}
