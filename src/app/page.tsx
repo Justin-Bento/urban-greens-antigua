ce21ec26101c633815cc6ec528b38f2e92efc06c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-11 18:31:55 -0400

    Added section two which talks about the features of microgreens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,12 +37,74 @@ export default function Page() {
         {/*
          * End of websites hero section.
          */}
-        <section className="">
-          <h2 className="scroll-m-20 pb-2 text-3xl font-semibold first:mt-0">
-            Hello Section #2
-          </h2>
-          <p className=""></p>
+        <section className="space-y-32">
+          <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-16">
+            <div className="">
+              <h2 className="scroll-m-20 pb-2 text-3xl font-semibold first:mt-0">
+                Freshly Grown In Antigua
+              </h2>
+              <p className="max-w-prose text-balance">
+                Most of our plants are harvested in a ten-day to a two-week
+                cycle. We grow and harvest our greens in a controlled
+                environment, without the use of chemicals or pesticides, to
+                ensure a healthy and nutritional product.
+              </p>
+            </div>
+            <AspectRatio ratio={16 / 16} className="bg-muted">
+              <Image
+                src="/media/freshly-grown.png"
+                alt="Photo by Drew Beamer"
+                fill
+                className="rounded-md object-cover"
+              />
+            </AspectRatio>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-16">
+            <div className="lg:order-2">
+              <h2 className="scroll-m-20 pb-2 text-3xl font-semibold first:mt-0">
+                A Tiny Ingredient with Big Uses
+              </h2>
+              <p className="max-w-prose text-balance">
+                Enjoy them on their own or mix them into salads and smoothies
+                for a nutrient boost. They make fantastic toppers for sandwiches
+                and add a burst of flavor as a garnish or decoration for your
+                plates.
+              </p>
+            </div>
+            <AspectRatio ratio={16 / 16} className="bg-muted">
+              <Image
+                src="/media/microgreens-variety.png"
+                alt="Photo by Drew Beamer"
+                fill
+                className="rounded-md object-cover"
+              />
+            </AspectRatio>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-16">
+            <div className="">
+              <h2 className="scroll-m-20 pb-2 text-3xl font-semibold first:mt-0">
+                Freshly Grown In Antigua
+              </h2>
+              <p className="max-w-prose text-balance">
+                Most of our plants are harvested in a ten-day to a two-week
+                cycle. We grow and harvest our greens in a controlled
+                environment, without the use of chemicals or pesticides, to
+                ensure a healthy and nutritional product.
+              </p>
+            </div>
+            <AspectRatio ratio={16 / 16} className="bg-muted">
+              <Image
+                src="/media/sustainable-packageing.png"
+                alt="Photo by Drew Beamer"
+                fill
+                className="rounded-md object-cover"
+              />
+            </AspectRatio>
+          </div>
         </section>
+        {/*
+         * End of websites hero section.
+         */}
         <section className="">
           <h3 className="scroll-m-20 pb-2 text-3xl font-semibold first:mt-0">
             Hello Section #3
