7ff0072ae62394269f321198c9009dac38cd9f76
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-12 00:23:50 -0400

    Updated the font from geist to public sans

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Public_Sans, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const geistPublicSans = Public_Sans({
+  variable: "--font-Public_Sans",
   subsets: ["latin"],
 });
 
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistPublicSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
