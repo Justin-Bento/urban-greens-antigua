5b185c1ad825e8e9afa0550f25f622eb791453fd
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-12 00:24:21 -0400

    Completed navigation for website

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,71 @@
-import React from "react";
-import { Card, CardContent } from "./ui/card";
+"use client";
+import React, { useState } from "react";
+import { Card, CardContent, CardDescription } from "./ui/card";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { RiCloseLine, RiExternalLinkLine, RiMenuLine } from "react-icons/ri";
+import Image from "next/image";
 
-export default function Navigation() {
+export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false); // Set initial state to false (closed)
+  const toggleMobileMenu = () => setMobileMenuOpen((prev) => !prev);
+
+  const navigation = ["home", "about", "products", "questions", "contact"];
+
   return (
-    <header className="sticky top-0 z-90">
+    <header className="sticky top-0 z-10">
       <Card className="rounded-none">
-        <CardContent className="container mx-auto">
-          <p className="">Start of the website.</p>
+        <CardContent className="container mx-auto grid grid-cols-1 items-center gap-4 lg:grid-cols-2">
+          {/* Logo and Mobile Menu Toggle */}
+          <div className="flex w-full items-center justify-between">
+            <div className="relative w-24 aspect-[16/11]">
+              <Image
+                fill
+                src="/media/logo/urban-greens-logo.png"
+                alt=""
+                className="object-center"
+              />
+            </div>
+            <Button
+              variant="outline"
+              size="icon"
+              className="lg:hidden dark:text-white"
+              onClick={toggleMobileMenu}
+              aria-label={
+                mobileMenuOpen ? "nav-button-close" : "nav-button-open"
+              }
+            >
+              {mobileMenuOpen ? <RiCloseLine /> : <RiMenuLine />}
+            </Button>
+          </div>
+
+          {/* Navigation Links */}
+          <div
+            className={`${
+              mobileMenuOpen ? "block" : "hidden"
+            } lg:block lg:flex lg:items-center lg:justify-end`}
+          >
+            <ul
+              role="list"
+              className="flex flex-col gap-4 lg:flex-row lg:items-center lg:justify-end"
+            >
+              {navigation.map((navigator, index) => (
+                <li key={index}>
+                  <Link href={navigator === "home" ? "/" : `/${navigator}`}>
+                    <Button
+                      variant="link"
+                      size="sm"
+                      className="w-full justify-start lg:justify-center hover:cursor-pointer"
+                    >
+                      <CardDescription className="capitalize text-black dark:text-white">
+                        {navigator}
+                      </CardDescription>
+                    </Button>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
         </CardContent>
       </Card>
     </header>
