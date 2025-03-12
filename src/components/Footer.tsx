5e0b83c2bf7cd0f40cd3fc0894589ba257ecb2c5
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-12 00:24:07 -0400

    Replaced paragraph with company logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Card, CardContent } from "./ui/card";
+import Image from "next/image";
 
 export default function Footer() {
   return (
     <footer className="">
-      <Card className="bg-gray-900 text-white rounded-none shadow-none border-none">
+      <Card className="bg-green-900 text-white rounded-none shadow-none border-none">
         <CardContent className="container mx-auto">
-          <p className="">End of the website.</p>
+          <div className="relative w-32 aspect-[16/11]">
+            <Image
+              fill
+              src="/media/logo/urban-greens-logo-white.png"
+              alt=""
+              className="object-center"
+            />
+          </div>
         </CardContent>
       </Card>
     </footer>
