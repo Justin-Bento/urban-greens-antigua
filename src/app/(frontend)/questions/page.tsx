deeb9a159ef285df01d1c5a634dab92a4bde95f3
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-12 02:16:06 -0400

    Created a page called questions

---
+++ src/app/(frontend)/questions/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      <section className="">
+        <h1 className="">Hello, Next.js!</h1>
+        <p className="">
+          This page is called <b>questions</b>
+        </p>
+      </section>
+    </>
+  );
+}
