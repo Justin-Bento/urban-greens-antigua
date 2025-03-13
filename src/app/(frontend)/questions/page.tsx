60257374e4e8ed7546a5615f204bf910992b348c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-12 21:23:46 -0400

    Created a frequenly asked question pages that has an accordian

--- src/app/(frontend)/questions/page.tsx
+++ src/app/(frontend)/questions/page.tsx
@@ -1,12 +1,14 @@
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardTitle,
-} from "@/components/ui/card";
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
 import { Separator } from "@/components/ui/separator";
 import { sanityFetch } from "@/sanity/lib/live";
 import { All_Questions } from "@/sanity/lib/queries";
+import { PortableText } from "next-sanity";
 import React from "react";
 
 export default async function page() {
@@ -25,16 +27,20 @@ export default async function page() {
           you're lucky someone will get back to you.
         </p>
         <Separator className="max-w-[40ch] mt-12" />
-        <ul className="">
+        <ul className="space-y-8 mt-16">
           {questions.map((question: any) => {
             return (
               <li key={question?._id}>
-                <Card>
-                  <CardContent>
-                    <CardTitle>{question?.question}</CardTitle>
-                    <CardDescription>{question?.details}</CardDescription>
-                  </CardContent>
-                </Card>
+                <Accordion type="single" collapsible>
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger className="text-xl hover:cursor-pointer">
+                      Qusetion - {question.question}
+                    </AccordionTrigger>
+                    <AccordionContent className="prose text-balance text-muted-foreground">
+                      <PortableText value={question.details} />
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
               </li>
             );
           })}
