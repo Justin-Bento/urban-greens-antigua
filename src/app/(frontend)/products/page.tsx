2f8f1534f10067287081aedabc8ca1948f855188
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-14 19:15:02 -0400

    Removed any type

--- src/app/(frontend)/products/page.tsx
+++ src/app/(frontend)/products/page.tsx
@@ -32,7 +32,7 @@ export default async function Page() {
         <Separator className="max-w-[40ch] mt-12" />
       </div>
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {posts.map((post: any) => (
+        {posts.map((post) => (
           <li key={post._id}>
             <Link href={`/products/${post?.slug?.current}`}>
               <Card className="shadow-none group hover:bg-muted transition-all">
