0c2ee73f64eb22d8d16da12416c0b7109b661b66
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-15 15:04:10 -0400

    Added supported types to questions page.

--- src/app/(frontend)/questions/page.tsx
+++ src/app/(frontend)/questions/page.tsx
@@ -11,6 +11,12 @@ import { All_Questions } from "@/sanity/lib/queries";
 import { PortableText } from "next-sanity";
 import React from "react";
 
+interface AllQuestionsInterface {
+  _id: string;
+  question: string;
+  details: [];
+}
+
 export default async function page() {
   const { data: questions } = await sanityFetch({ query: All_Questions });
 
@@ -29,7 +35,7 @@ export default async function page() {
         </p>
         <Separator className="max-w-[40ch] mt-12" />
         <ul className="space-y-8 mt-16">
-          {questions?.map((question) => (
+          {questions?.map((question: AllQuestionsInterface) => (
             <li key={question?._id}>
               <Accordion type="single" collapsible>
                 <AccordionItem value={question._id}>
