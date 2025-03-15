aaa889408f19241e704c6a286604af5edc665949
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-15 00:26:42 -0400

    Completed content for the about page.

--- src/app/(frontend)/about/page.tsx
+++ src/app/(frontend)/about/page.tsx
@@ -1,8 +1,16 @@
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { values } from "@/lib/data";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { RiExternalLinkLine } from "react-icons/ri";
 
 export default function page() {
   return (
@@ -14,42 +22,48 @@ export default function page() {
             We love Microgreens
           </h1>
           <p className="mt-4 text-balance text-lg font-medium sm:text-xl/8 max-w-[90ch]">
-            Urban Greens Antigua started out as a passion project that quickly
-            evolved into a business. We realized that our customers wanted to
-            experience and enjoy a fresh way of using micro greens.
+            Urban Greens Antigua began as a passion project and quickly grew
+            into a thriving business. We discovered that our customers were
+            eager to explore and enjoy a fresh, innovative way of incorporating
+            vegtables into their daily lives.
           </p>
         </div>
         {/* Content section */}
         <div className="grid max-w-xl grid-cols-1 gap-8 text-base/7 opacity-60 lg:max-w-none lg:grid-cols-2">
           <div>
             <p>
-              Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-              enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-              praesent donec est. Odio penatibus risus viverra tellus varius sit
-              neque erat velit. Faucibus commodo massa rhoncus, volutpat.
-              Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae
-              sed turpis id.
+              Urban Greens Antigua began as a passion project, born out of a
+              love for fresh, sustainable, and nutrient-rich food. What started
+              as a small experiment quickly grew into a thriving business as we
+              discovered the growing demand for microgreens in our community.
+              Today, we’re proud to offer a fresh, innovative way to enjoy these
+              tiny but mighty greens.
             </p>
             <p className="mt-8">
-              Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
-              odio id et. Id blandit molestie auctor fermentum dignissim. Lacus
-              diam tincidunt ac cursus in vel. Mauris varius vulputate et
-              ultrices hac adipiscing egestas.
+              Our microgreens are grown with care, using sustainable practices
+              that prioritize quality and freshness. Packed with vitamins,
+              minerals, and antioxidants, they offer up to 40 times more
+              nutrients than their mature counterparts. From peppery arugula to
+              delicate radish sprouts, each variety brings its own unique flavor
+              and nutritional benefits to the table..
             </p>
           </div>
           <div>
             <p>
-              Erat pellentesque dictumst ligula porttitor risus eget et eget.
-              Ultricies tellus felis id dignissim eget. Est augue maecenas risus
-              nulla ultrices congue nunc tortor. Enim et nesciunt doloremque
-              nesciunt voluptate.
+              We realized that our customers were eager to explore new ways to
+              incorporate microgreens into their daily lives. Whether it’s
+              adding a burst of flavor to salads, garnishing gourmet dishes, or
+              blending them into smoothies, microgreens have become a versatile
+              staple for health-conscious individuals and culinary enthusiasts
+              alike.
             </p>
             <p className="mt-8">
-              Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis
-              odio id et. Id blandit molestie auctor fermentum dignissim. Lacus
-              diam tincidunt ac cursus in vel. Mauris varius vulputate et
-              ultrices hac adipiscing egestas. Iaculis convallis ac tempor et
-              ut. Ac lorem vel integer orci.
+              Join us on this journey toward healthier, more sustainable living.
+              Whether you’re a home cook, a professional chef, or simply curious
+              about the benefits of microgreens, Urban Greens Antigua is here to
+              provide you with the freshest, most flavorful greens. Explore our
+              range and discover how these tiny greens can make a big impact on
+              your health and culinary creations!
             </p>
           </div>
         </div>
@@ -66,62 +80,58 @@ export default function page() {
         {/* Values section */}
         <div className="">
           <div className="mx-auto max-w-2xl lg:mx-0">
-            <h2 className="text-pretty text-4xl font-semibold tracking-tight sm:text-5xl">
-              Our values
+            <h2 className="text-pretty text-4xl font-semibold tracking-tight sm:text-5xl capitalize">
+              We’d love to hear from you!
             </h2>
             <p className="mt-6 text-lg/8">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
-              impedit perferendis suscipit eaque, iste dolor cupiditate
-              blanditiis.
+              Share your thoughts, recipes, or feedback with us, and let’s grow
+              together. Your ideas inspire us to keep innovating and delivering
+              the best for our community.
             </p>
           </div>
-          <dl className="mx-auto mt-16 grid grid-cols-1 gap-8 text-base/7 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-16">
-            {values.map((value) => (
-              <div key={value.name} className="relative pl-9">
-                <dt className="inline font-semibold">
-                  <value.icon
-                    aria-hidden="true"
-                    className="absolute left-1 top-1 size-5 text-green-500"
-                  />
-                  {value.name}
-                </dt>{" "}
-                <dd className="inline">{value.description}</dd>
-              </div>
-            ))}
-          </dl>
-        </div>
-        <div className="container mx-auto sm:px-6">
-          <h2 className="text-pretty text-4xl font-semibold tracking-tight sm:text-5xl mb-6 capitalize">
-            Show people how you use Microgreens!
-          </h2>
-          <p className="leading-7 text-lg">
-            Follow our community and stay up today to see how people are using
-            microgreens in their Lifestyle.
-          </p>
           <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-8 mt-8">
-            <div className="w-full space-y-6 ">
-              <div className="">
-                <h3 className="text-lg font-semibold mb-2">
-                  Visit Our Facebook Microgreens Group!
-                </h3>
-                <p className="">
-                  Got something to share or waiting for our latest update?
-                  Interact with our online community, and see how people from
-                  all backgrounds are using our greens.
-                </p>
-              </div>
-              <Separator className="border border-green-700 w-20" />
-              <div className="">
-                <h3 className="text-lg font-semibold mb-2">
-                  Visit Our Facebook Microgreens Group!
-                </h3>
-                <p className="">
-                  Got something to share or waiting for our latest update?
-                  Interact with our online community, and see how people from
-                  all backgrounds are using our greens.
-                </p>
-              </div>
-            </div>
+            <Link
+              href="https://www.instagram.com/urbangreensanu/"
+              className="group"
+            >
+              <Card className="shadow-none hover:bg-gray-50">
+                <CardContent className="space-y-4">
+                  <CardTitle className="text-lg font-semibold mb-2">
+                    Check Out Our Facebook Page!
+                  </CardTitle>
+                  <CardDescription className="text-base/7">
+                    Got something to share or waiting for our latest update?
+                    Interact with our online community, and see how people from
+                    all backgrounds are using our greens.
+                  </CardDescription>
+                  <div className="text-sm text-muted-background opacity-70 flex items-center gap-2 group-hover:underline hover">
+                    <RiExternalLinkLine />
+                    <span className="block font-mono">Visit Our Page</span>
+                  </div>
+                </CardContent>
+              </Card>
+            </Link>
+            <Link
+              href="https://www.instagram.com/urbangreensanu/"
+              className="group"
+            >
+              <Card className="shadow-none hover:bg-gray-50">
+                <CardContent className="space-y-4">
+                  <CardTitle className="text-lg font-semibold mb-2">
+                    Check Out Our Instagram Page!
+                  </CardTitle>
+                  <CardDescription className="text-base/7">
+                    Got something to share or waiting for our latest update?
+                    Interact with our online community, and see how people from
+                    all backgrounds are using our greens.
+                  </CardDescription>
+                  <div className="text-sm text-muted-background opacity-70 flex items-center gap-2 group-hover:underline hover">
+                    <RiExternalLinkLine />
+                    <span className="block font-mono">Visit Our Page</span>
+                  </div>
+                </CardContent>
+              </Card>
+            </Link>
           </div>
         </div>
       </section>
