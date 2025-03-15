f2535a0b178048cef57e306c14cf1f76a9d41aa8
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-03-15 15:28:08 -0400

    Removed custom types and utilizsed sanity tyapes

--- src/app/(frontend)/products/page.tsx
+++ src/app/(frontend)/products/page.tsx
@@ -12,15 +12,6 @@ import Image from "next/image";
 import { Separator } from "@/components/ui/separator";
 import { urlFor } from "@/sanity/lib/image";
 
-interface ProductsInterface {
-  _id: string;
-  slug: {
-    current: string;
-  };
-  mainImage: string;
-  name: string;
-  description: string;
-}
 export default async function Page() {
   const { data: posts } = await sanityFetch({ query: POSTS_QUERY });
 
@@ -42,7 +33,7 @@ export default async function Page() {
         <Separator className="max-w-[40ch] mt-12" />
       </div>
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {posts.map((post: ProductsInterface) => (
+        {posts.map((post) => (
           <li key={post._id}>
             <Link href={`/products/${post?.slug?.current}`}>
               <Card className="shadow-none group hover:bg-muted transition-all">
--- src/app/(frontend)/questions/page.tsx
+++ src/app/(frontend)/questions/page.tsx
@@ -11,12 +11,6 @@ import { All_Questions } from "@/sanity/lib/queries";
 import { PortableText } from "next-sanity";
 import React from "react";
 
-interface AllQuestionsInterface {
-  _id: string;
-  question: string;
-  details: [];
-}
-
 export default async function page() {
   const { data: questions } = await sanityFetch({ query: All_Questions });
 
@@ -35,7 +29,7 @@ export default async function page() {
         </p>
         <Separator className="max-w-[40ch] mt-12" />
         <ul className="space-y-8 mt-16">
-          {questions?.map((question: AllQuestionsInterface) => (
+          {questions?.map((question) => (
             <li key={question?._id}>
               <Accordion type="single" collapsible>
                 <AccordionItem value={question._id}>
