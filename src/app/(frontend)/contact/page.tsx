6bfc129939cb6e018067c6e9a637faf3ab318c7b
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-30 21:41:04 -0400

    Updated the typography balance

--- src/app/(frontend)/contact/page.tsx
+++ src/app/(frontend)/contact/page.tsx
@@ -11,12 +11,13 @@ export default function page() {
         <section className="">
           <div className="">
             <span className="space-y-1">
-              <h1 className="scroll-m-20 text-4xl font-bold capitalize lg:text-5xl">
+              <h1 className="scroll-m-20 text-4xl font-bold capitalize">
                 Contact Us.
               </h1>
             </span>
-            <p className="max-w-[120ch] opacity-70 text-balance my-4 text-lg">
-              By droping us a message and we will get back to you.
+            <p className="text-balance text-foreground my-4 text-lg/[1.75]">
+              Please feel free to drop us a message, and we'll get back to you
+              promptly.
             </p>
             <Separator className="max-w-[40ch] my-12" />
           </div>
