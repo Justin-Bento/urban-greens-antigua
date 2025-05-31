f275412af2b26bef111c257d7937202da91bc9e7
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-30 21:18:42 -0400

    Added custom line height of 1.5

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
         <h1 className="text-5xl font-bold text-foreground tracking-tight text-balance">
           Sustainable Microgreens, Unbeatable Flavor!
         </h1>
-        <p className="text-xl">
+        <p className="text-xl/[1.5]">
           Microgreens are vegetable greens that are packed with nutrients and
           flavor, making them the perfect addition to any meal. From salads to
           smoothies, sandwiches to sushi, the possibilities are endless. Plus,
