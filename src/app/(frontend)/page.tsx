16068a59aab14567fcca3362335dcfda9c46652a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-05-30 21:19:32 -0400

    Applied text balance to all elements inside the hero section.

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -14,8 +14,8 @@ import Link from "next/link";
 export default function Page() {
   return (
     <>
-      <section className="flex flex-col lg:items-center gap-6 text-center">
-        <h1 className="text-5xl font-bold text-foreground tracking-tight text-balance">
+      <section className="flex flex-col lg:items-center gap-6 text-center [&>*]:text-balance">
+        <h1 className="text-5xl font-bold text-foreground tracking-tight">
           Sustainable Microgreens, Unbeatable Flavor!
         </h1>
         <p className="text-xl/[1.5]">
