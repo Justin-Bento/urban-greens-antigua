99736a727b8bc889fb659543dcf7392283829196
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-31 12:30:30 -0400

    Added shadow and border on outside box.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -9,11 +9,11 @@ export default function Navigation() {
   };
 
   return (
-    <div>
+    <header className="py-6 shadow border-b border-black/10">
       <button onClick={handleToggle}>{isToggled ? "ON" : "OFF"}</button>
 
       {/* Conditionally render content */}
       {isToggled && <p>This content is visible when toggled ON.</p>}
-    </div>
+    </header>
   );
 }
