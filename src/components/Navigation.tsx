31d8e0e37668c7beee82b3d50dee980eee83d4cb
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-12-31 12:34:47 -0400

    Applied brand logo to header.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { twMerge } from "tailwind-merge";
+import Logo from "./Logo";
 
 export default function Navigation() {
   const [isToggled, setIsToggled] = useState(false);
@@ -13,6 +14,7 @@ export default function Navigation() {
     <header className="py-6 shadow border-b border-black/10">
       <div className="wrapper">
         <div className="">
+          <Logo />
           <button
             onClick={handleToggle}
             className={twMerge("border border-black", "py-2 px-6")}
